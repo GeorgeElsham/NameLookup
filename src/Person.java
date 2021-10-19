21bc3e4d109b5b7deaafbe280b3ef055c248c93e
GeorgeElsham <40073010+GeorgeElsham@users.noreply.github.com>
2021-10-19 07:18:27 +0100

    Easy way to print details about person

--- src/Person.java
+++ src/Person.java
@@ -1,11 +1,17 @@
 public class Person {
-  String name;
-  String email;
-  String telephone;
+  public String name;
+  public String email;
+  public String telephone;
 
   public Person(String name, String email, String telephone) {
     this.name = name;
     this.email = email;
     this.telephone = telephone;
+  }
+
+  public void printDetails() {
+    System.out.println("Name: " + name);
+    System.out.println("Email: " + email);
+    System.out.println("Telephone: " + name);
   }
 }
