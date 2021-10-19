28f78abceb9c6956294a55227d817cde3b15c6c0
GeorgeElsham <40073010+GeorgeElsham@users.noreply.github.com>
2021-10-19 07:19:25 +0100

    Username is inputted and validated

--- src/App.java
+++ src/App.java
@@ -1,7 +1,47 @@
+import java.util.*;
+
 public class App {
   public static void main(String[] args) {
+    final App app = new App();
+    app.run();
+  }
+
+  private void run() {
     final User user = new User();
-    final Person person = user.query("pll");
-    System.out.println(person.name);
+    askForUsername(user);
+  }
+
+  private void askForUsername(User user) {
+    // Get username input
+    final String username = input("Enter username (e.g. pll): ");
+
+    // Validate username
+    if (isValidUsername(username)) {
+      // Query and print results
+      final Person person = user.query(username);
+
+      // Check person exists
+      if (person != null) {
+        person.printDetails();
+      } else {
+        System.out.println("Username does not exist.");
+      }
+    } else {
+      // Invalid username
+      final String msg = String.format("Invalid username query '%s'.", username);
+      System.out.println(msg);
+    }
+  }
+
+  private String input(String message) {
+    System.out.print(message);
+    final Scanner scanner = new Scanner(System.in);
+    final String line = scanner.nextLine();
+    System.out.println();
+    return line;
+  }
+
+  private boolean isValidUsername(String username) {
+    return username.matches("^[a-z0-9]+$");
   }
 }
