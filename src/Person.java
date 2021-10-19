3ac8909f71808fea4eacff68472ebf64ea1b6fa4
GeorgeElsham <40073010+GeorgeElsham@users.noreply.github.com>
2021-10-19 06:45:16 +0100

    Full `Person` object

---
+++ src/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+  String name;
+  String email;
+  String telephone;
+
+  public Person(String name, String email, String telephone) {
+    this.name = name;
+    this.email = email;
+    this.telephone = telephone;
+  }
+}
